d0a4e10f725adc4ac7db6a11f462d4a9cc48e117
Кириенко Виктор Евгеньевич <16480@montagg.ru>
2020-03-02 15:21:15 +0500

    asdf

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,3 +3,4 @@ require_once "app/db/components/Connection.php";
 require_once "app/models/PostData.php";
 $config = require_once "config.php";
 $newPost = new PostData(Connection::make($config));
+//hello boyyyyyy
